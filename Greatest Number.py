f1f71a5de33a9e4a31ec103989d44e8d9e7b51a2
Tamrazrk <135056938+Tamrazrk@users.noreply.github.com>
2023-07-26 11:10:15 +0400

    Create Greatest Number.py

---
+++ Greatest Number.py
@@ -0,0 +1,10 @@
+# Ask the user for 3 numbers
+num1 = float(input("Input the 1st number: "))
+num2 = float(input("Input the 2nd number: "))
+num3 = float(input("Input the 3rd number: "))
+
+# Find the greatest number among the three
+greatest_number = max(num1, num2, num3)
+
+# Print the greatest number
+print("The greatest number is:", greatest_number)
